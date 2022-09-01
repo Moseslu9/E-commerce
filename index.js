da037cc20cc8e02738eff7754422d27586e59207
Tesla_Ambassador <77091499+tesla-ambassador@users.noreply.github.com>
2022-09-01 16:49:49 +0300

    Helped with Js
    Helped make the thumbnails able to display the image in image viewer

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 // Get the modal
-var modal = document.getElementById("myModal");
-var thumbs = document.getElementById('clickable');
+const modal = document.getElementById("myModal");
+const thumbs = document.querySelectorAll('.clickable');
+const imgViewer = document.querySelector('#img01')
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
+const img = document.getElementById("myImg");
+const modalImg = document.getElementById("img01");
 img.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.src;
 }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
+//Array of img Urls.
+
+const images = ['./images/image-product-1.jpg', './images/image-product-2.jpg', './images/image-product-3.jpg', './images/image-product-4.jpg']
+
+thumbs.forEach((thumb, index) => {
+  thumb.addEventListener('click', () => {
+    imgViewer.setAttribute('src', images[index])
+  })
+})
